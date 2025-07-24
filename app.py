0c341788185ac8786f7605000cf504e792db7291
Spicysimonroll <simon.hu.1999@gmail.com>
2025-07-24 13:39:35 +0200

    push

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+import os
+import socket
 
 app = Flask(__name__)
 CORS(app)
@@ -7,6 +9,12 @@ CORS(app)
 @app.route('/')
 def hello():
     return jsonify(message = 'Hello from K8S')
+
+@app.route('/hostname')
+def get_hostname():
+    msg = os.getenv('MESSAGE', 'Hello from K8S')
+    hostname = socket.gethostname()
+    return jsonify(hostname = hostname, message = msg)
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0', port = 5000, debug = True)
